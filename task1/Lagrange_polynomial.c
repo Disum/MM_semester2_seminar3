fbf6faa08666590e8ded2a017857db975755b69a
yakov <mirenkovye@yandex.ru>
2020-02-24 04:59:42 +0300

    Minor changes

--- task1/Lagrange_polynomial.c
+++ task1/Lagrange_polynomial.c
@@ -3,20 +3,20 @@
 double Lagrange_polynomial(int n, double x0, double *x, double *y)
 {
 	int i, j;
-	double basic_polynomial, res = 0;;
+	double term, res = 0;;
 
 	for( i = 0; i<n; i++ )
 	{
-		basic_polynomial = 1;
+		term = y[i];
 		for( j = 0; j<n; j++ )
 		{
 			if( i==j )
 				continue;
 
-			basic_polynomial *= (x0 - x[j])/(x[i] - x[j]);
+			term *= (x0 - x[j])/(x[i] - x[j]);
 		}
 
-		res += y[i]*basic_polynomial;
+		res += term;
 	}
 
 	return res;
