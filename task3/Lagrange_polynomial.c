12d675117ab20dc738ae4450056b7a91bd2fb671
yakov <mirenkovye@yandex.ru>
2020-02-28 05:01:59 +0300

    Fixed bugs

--- task3/Lagrange_polynomial.c
+++ task3/Lagrange_polynomial.c
@@ -4,7 +4,7 @@ double Lagrange_polynomial(int n, double x0, double *x, double *y)
 {
 	int i, j;
 
-	for( i = 0; i<n; i++ )
+	for( i = 0; i<(n - 1); i++ )
 	{
 		for( j = 0; j<(n - i - 1); j++ )
 			y[j] = ( (x0 - x[j])*y[j + 1] - (x0 - x[j + i + 1])*y[j] )/(x[j + i + 1] - x[j]);
