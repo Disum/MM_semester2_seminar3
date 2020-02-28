460ea9b377ac888be6e468a8b57c7eced3488769
yakov <mirenkovye@yandex.ru>
2020-02-28 04:03:55 +0300

    Fixed input

--- task1/main.c
+++ task1/main.c
@@ -10,12 +10,12 @@ int main(int argc, const char **argv)
 {
 	double x0, *x, *y, res;
 	int n, i;
-	FILE *x_arr, *y_arr;
+	FILE *input;
 	clock_t time_beg;
 
-	if( argc!=4 )
+	if( argc!=3 )
 	{
-		fprintf(stderr, "Usage: %s n x_array_file y_array_file\n", argv[0]);
+		fprintf(stderr, "Usage: %s n input_file\n", argv[0]);
 		return ERROR_READ;
 	}
 	if( sscanf(argv[1], "%d", &n)!=1 || n<2)
@@ -29,49 +29,32 @@ int main(int argc, const char **argv)
 		fprintf(stderr, "Error! Can not allocate memory!\n");
 		return ERROR_ALLOCATE;
 	}
-	if( !(x_arr = fopen(argv[2], "r")) )
-	{
-		fprintf(stderr, "Error! Can not open %s!\n", argv[2]);
-		free(x);
-		return ERROR_OPEN;
-	}
-	for( i = 0; i<n; i++ )
-	{
-		if( fscanf(x_arr, "%lf", x + i)!=1 )
-		{
-			fprintf(stderr, "Error! Can not read element number %d from %s!", i + 1, argv[2]);
-			free(x);
-			fclose(x_arr);
-			return ERROR_READ;
-		}
-	}
-	fclose(x_arr);
-
 	if( !(y = (double *)malloc(n*sizeof(double))) )
 	{
 		fprintf(stderr, "Error! Can not allocate memory!\n");
 		free(x);
 		return ERROR_ALLOCATE;
 	}
-	if( !(y_arr = fopen(argv[3], "r")) )
+
+	if( !(input = fopen(argv[2], "r")) )
 	{
-		fprintf(stderr, "Error! Can not open %s!\n", argv[3]);
+		fprintf(stderr, "Error! Can not open %s!\n", argv[2]);
 		free(x);
-		free(y);
 		return ERROR_OPEN;
 	}
+
 	for( i = 0; i<n; i++ )
 	{
-		if( fscanf(y_arr, "%lf", y + i)!=1 )
+		if( fscanf(input, "%lf", x + i)!=1 || fscanf(input, "%lf", y + i)!=1  )
 		{
-			fprintf(stderr, "Error! Can not read element number %d from %s!", i + 1, argv[3]);
+			fprintf(stderr, "Error! Can not read element from %s!", argv[2]);
 			free(x);
 			free(y);
-			fclose(y_arr);
+			fclose(input);
 			return ERROR_READ;
 		}
 	}
-	fclose(y_arr);
+	fclose(input);
 
 	printf("x0 = ");
 	if( scanf("%lf", &x0)!=1 )
